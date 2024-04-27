7c4d764b6d44d5023ee9b29ea076e05023c4904c
baddestsupercec <connor.callan28@yahoo.com>
2024-04-27 13:56:01 -0500

    Revert some issues

--- src/ui-components/index.js
+++ src/ui-components/index.js
@@ -4,10 +4,6 @@
  * Any changes to this file will be overwritten when running amplify pull. *
  **************************************************************************/
 
-export { default as HealthParametersCreateForm } from "./HealthParametersCreateForm";
-export { default as HealthParametersUpdateForm } from "./HealthParametersUpdateForm";
 export { default as NoteCreateForm } from "./NoteCreateForm";
 export { default as NoteUpdateForm } from "./NoteUpdateForm";
-export { default as UserDataCreateForm } from "./UserDataCreateForm";
-export { default as UserDataUpdateForm } from "./UserDataUpdateForm";
 export { default as studioTheme } from "./studioTheme";
